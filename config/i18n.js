be4d1736e97459a747efbe950058249f08218b8a
Andrey <systerr@gmail.com>
2021-10-22 10:04:32 +0800

    typo

--- config/i18n.js
+++ config/i18n.js
@@ -4,6 +4,6 @@ module.exports = {
   fallbackLng: 'en',
   // https://github.com/i18next/i18next-http-middleware#detector-options
   // in additional we have 'xLang' that detect language based on header 'xLang'
-  langDetectionOders: ['xLang'], // from oreder option
+  langDetectionOders: ['xLang'], // from order option
   lookupQuerystring: 'lng', // string to detect language on query
 };
