d4a4af6441823bba619024db789cc6e07bc0348a
Banancly <banancly@gmail.com>
2021-11-12 18:31:22 +0300

    reg test

--- tests/setup.js
+++ tests/setup.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 let mongoMemoryServerInstance;
 const path = require('path');
+const SomeController = require('../controllers/test/SomeController');
 const Server = require('../server');
 
 jest.setTimeout(1000000);
@@ -60,7 +61,13 @@ beforeAll(async () => {
   if (typeof global.testSetup.beforeAll === 'function') {
     await global.testSetup.beforeAll();
   }
+
   await global.server.startServer();
+
+  global.server.controllers['/SomeController'] = new SomeController(
+    global.server.app,
+    '/SomeController',
+  );
 });
 afterAll(async () => {
   if (global.server) {
