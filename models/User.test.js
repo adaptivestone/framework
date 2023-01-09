c5761786d0e872082ff336209ac29ce20fbab20b
Andrey <systerr@gmail.com>
2023-01-09 18:26:43 +0800

    more tests

--- models/User.test.js
+++ models/User.test.js
@@ -71,6 +71,26 @@ describe('user model', () => {
     });
   });
 
+  describe('getUserByToken', () => {
+    it('should NOT work for non valid token', async () => {
+      expect.assertions(1);
+
+      const user = await global.server.app
+        .getModel('User')
+        .getUserByToken('fake one');
+      expect(user).toBe(false);
+    });
+
+    it('should  work for VALID token', async () => {
+      expect.assertions(1);
+      const token = await globalUser.generateToken();
+      const user = await global.server.app
+        .getModel('User')
+        .getUserByToken(token.token);
+      expect(user.id).toBe(globalUser.id);
+    });
+  });
+
   describe('getUserByVerificationToken', () => {
     it('should NOT work for non valid token', async () => {
       expect.assertions(2);
