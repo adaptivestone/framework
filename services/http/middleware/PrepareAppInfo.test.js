f491b99adaae20a223e97359728af219f56df742
Andrey <systerr@gmail.com>
2023-01-25 19:49:40 +0800

    app info test update

--- services/http/middleware/PrepareAppInfo.test.js
+++ services/http/middleware/PrepareAppInfo.test.js
@@ -7,16 +7,16 @@ describe('prepareAppInfo methods', () => {
     expect(middleware.constructor.description).toBeDefined();
   });
 
-  it('have description middleware that works', async () => {
+  it('middleware that works', async () => {
     expect.assertions(3);
     const middleware = new PrepareAppInfo(global.server.app);
     const nextFunction = jest.fn(() => {});
     const req = {};
-    middleware.middleware(req, {}, nextFunction);
+    await middleware.middleware(req, {}, nextFunction);
     expect(nextFunction).toHaveBeenCalledWith();
     expect(req.appInfo).toBeDefined();
     req.appInfo.test = 5;
-    middleware.middleware(req, {}, nextFunction);
+    await middleware.middleware(req, {}, nextFunction);
     expect(req.appInfo.test).toBe(5);
   });
 });
