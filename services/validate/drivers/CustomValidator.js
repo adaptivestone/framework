3b138c1630e3d7fa21b8dfd41db172360ffcc3fa
Arteom Kirchenko Mac <banancly@gmail.comm>
2023-02-10 15:43:42 +0300

    fix

--- services/validate/drivers/CustomValidator.js
+++ services/validate/drivers/CustomValidator.js
@@ -1,40 +1,104 @@
+const yup = require('yup');
 const AbstractValidator = require('./AbstractValidator');
 
-class CustomValidator extends AbstractValidator {
-  async validateFields(data, { query, body, appInfo }) {
-    if (this.body) {
-      if (typeof this.body.validate !== 'function') {
-        this.logger.error('request.validate should be a function');
-      }
-    }
-
-    return this.body.validate(data, {
-      req: {
-        query,
-        body,
-        appInfo,
-      },
-    });
+class YupValidator extends AbstractValidator {
+  get fieldsInJsonFormat() {
+    return this.constructor.convertFieldsToJson(this.body);
   }
 
-  async castFields(data, { query, body, appInfo }) {
-    if (this.body) {
-      if (typeof this.body.cast !== 'function') {
+  static convertFieldsToJson(fields) {
+    const convertedFields = {};
+    const entries = Object.entries(fields.describe().fields);
+
+    if (!entries?.length) {
+      return convertedFields;
+    }
+    const requiredFields = [];
+
+    for (const [field, fieldProp] of entries) {
+      const isRequired = fieldProp?.tests?.find(
+        (prop) => prop.name === 'required',
+      );
+      if (isRequired) {
+        requiredFields.push(field);
+      }
+    }
+
+    entries.forEach(([key, value]) => {
+      if (!convertedFields[key]) {
+        convertedFields[key] = {};
+      }
+
+      convertedFields[key] = {
+        type: value.type,
+        required: requiredFields?.includes(key),
+      };
+    });
+
+    return convertedFields;
+  }
+
+  async validateFields(data, { query, body, appInfo }) {
+    const yupSchema = this.body;
+    const { controllerValidationAbortEarly } = this.app.getConfig('validate');
+    if (yupSchema) {
+      if (typeof yupSchema.validate !== 'function') {
         this.logger.error('request.validate should be a function');
       }
     }
 
-    return this.body.cast(data, {
-      req: {
-        query,
-        body,
-        appInfo,
-      },
+    try {
+      await yupSchema.validate(data, {
+        abortEarly: controllerValidationAbortEarly,
+        req: { query, body },
+      });
+    } catch (e) {
+      console.log(e);
+      let { errors } = e;
+      // translate it
+      if (appInfo.i18n && errors) {
+        errors = errors.map((err) => appInfo.i18n.t(err));
+      }
+      this.logger.error(
+        `Request validation failed with message: ${e.message}. errors: ${errors}`,
+      );
+
+      const errorAnswer = {};
+      if (!e.inner || !e.inner.length) {
+        errorAnswer[e.path] = errors;
+      } else {
+        e.inner.forEach((err) => {
+          errorAnswer[err.path] = err.errors;
+          if (appInfo.i18n && err.errors) {
+            errorAnswer[err.path] = err.errors.map((err1) =>
+              appInfo.i18n.t(err1),
+            );
+          }
+        });
+      }
+
+      throw new yup.ValidationError({
+        ...errorAnswer,
+      });
+    }
+  }
+
+  async castFields(data, { query, body }) {
+    const yupSchema = this.body;
+    if (yupSchema) {
+      if (typeof yupSchema.cast !== 'function') {
+        this.logger.error('request.cast should be a function');
+      }
+    }
+
+    return yupSchema.cast(data, {
+      stripUnknown: true,
+      req: { query, body },
     });
   }
 
   static get loggerGroup() {
-    return 'CustomValidator_';
+    return 'YupValidator_';
   }
 }
-module.exports = CustomValidator;
+module.exports = YupValidator;
