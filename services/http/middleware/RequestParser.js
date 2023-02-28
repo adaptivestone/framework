02f7db6c098a27b0b239b234f09533230d4ff6ec
Andrey <systerr@gmail.com>
2023-02-28 19:15:56 +0800

    add time

--- services/http/middleware/RequestParser.js
+++ services/http/middleware/RequestParser.js
@@ -8,11 +8,15 @@ class RequestParser extends AbstractMiddleware {
   }
 
   async middleware(req, res, next) {
+    const time = Date.now();
     this.logger.verbose(`Parsing request`);
+    // TODO update this to https://github.com/node-formidable/formidable/issues/412#issuecomment-1367914268 in node v20 (in 2023?)
 
     const form = formidable(this.params); // not in construstor as reuse formidable affects performance
     form.parse(req, (err, fields, files) => {
-      this.logger.verbose(`Parsing multipart/formdata request DONE`);
+      this.logger.verbose(
+        `Parsing multipart/formdata request DONE ${Date.now() - time}ms`,
+      );
       if (err) {
         this.logger.error(`Parsing failed ${err}`);
         return next(err);
