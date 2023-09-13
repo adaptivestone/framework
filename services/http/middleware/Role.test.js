93ba254bb3183eecf1865f8f1f6ac647333258fd
Andrey <systerr@gmail.com>
2023-09-14 01:44:45 +0400

    role test

---
+++ services/http/middleware/Role.test.js
@@ -0,0 +1,93 @@
+import { describe, it, expect } from 'vitest';
+import Role from './Role';
+
+describe('role middleware methods', () => {
+  it('have description fields', async () => {
+    expect.assertions(1);
+    const middleware = new Role(global.server.app);
+    expect(middleware.constructor.description).toBeDefined();
+  });
+
+  it('middleware pass when user presented with a right role', async () => {
+    expect.assertions(1);
+    let isCalled = false;
+    const nextFunction = () => {
+      isCalled = true;
+    };
+    const req = {
+      appInfo: {
+        user: {
+          roles: ['role1', 'role2'],
+        },
+      },
+    };
+    const middleware = new Role(global.server.app, {
+      roles: ['admin', 'role1'],
+    });
+
+    await middleware.middleware(req, {}, nextFunction);
+    expect(isCalled).toBeTruthy();
+  });
+
+  it('middleware NOT pass when user NOT presented', async () => {
+    expect.assertions(3);
+    let isCalled = false;
+    let status;
+    let isSend;
+    const nextFunction = () => {
+      isCalled = true;
+    };
+    const req = {
+      appInfo: {}, // no user
+    };
+    const middleware = new Role(global.server.app);
+    await middleware.middleware(
+      req,
+      {
+        status(statusCode) {
+          status = statusCode;
+          return this;
+        },
+        send() {
+          isSend = true;
+        },
+      },
+      nextFunction,
+    );
+    expect(isCalled).toBeFalsy();
+    expect(status).toBe(401);
+    expect(isSend).toBeTruthy();
+  });
+
+  it('middleware NOT pass when user  have a wrong role', async () => {
+    expect.assertions(3);
+    let isCalled = false;
+    let status;
+    let isSend;
+    const nextFunction = () => {
+      isCalled = true;
+    };
+    const req = {
+      appInfo: {
+        user: { roles: ['role1', 'role2'] },
+      },
+    };
+    const middleware = new Role(global.server.app, { roles: ['admin'] });
+    await middleware.middleware(
+      req,
+      {
+        status(statusCode) {
+          status = statusCode;
+          return this;
+        },
+        json() {
+          isSend = true;
+        },
+      },
+      nextFunction,
+    );
+    expect(isCalled).toBeFalsy();
+    expect(status).toBe(403);
+    expect(isSend).toBeTruthy();
+  });
+});
