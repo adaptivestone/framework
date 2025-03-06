fb95e8cbb52ea33525ffa2f175a9a8da491169db
Andrey <systerr@gmail.com>
2025-03-06 16:16:03 +0000

    rename file

--- vitest.config.js
+++ vitest.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
     globalSetup: './tests/globalSetupVitest.js',
     setupFiles: './tests/setupVitest.js',
     testTimeout: 10000,
-    outputFile: './coverage/rspec.xml',
+    outputFile: './coverage/junit.rspec.xml',
     reporters: ['default', 'junit'],
     coverage: {
       provider: 'v8',
       enabled: true,
-      reporter: ['text', 'html', 'clover', 'json', 'cobertura'],
+      reporter: ['text', 'html', 'clover', 'json'],
     },
   },
 });
