50090f2a99ab3a4468c69585cc1a73c5c5a08273
Andrey <systerr@gmail.com>
2025-04-16 20:41:10 +0100

    fix: update folderConfig import to use TypeScript file

--- cliCommand.js
+++ cliCommand.js
@@ -1,7 +1,7 @@
 /* eslint-disable import-x/first */
 console.time('CLI');
 import Cli from './src/Cli.js';
-import folderConfig from './src/folderConfig.js';
+import folderConfig from './src/folderConfig.ts';
 
 const cli = new Cli(folderConfig);
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Server from './server.js';
-import folderConfig from './folderConfig.js';
+import folderConfig from './folderConfig.ts';
 
 const server = new Server(folderConfig);
 
